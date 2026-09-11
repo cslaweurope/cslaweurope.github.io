8e8c92ce64af022781fc0df09c7c09b07af86791
lucas-ubm <lgubm23@gmail.com>
2026-09-11 06:56:08 +0000

    Deploying to gh-pages from @ cslaweurope/cslaweurope.github.io@d24b7ef326a5e24db97654af5b771fb6003507d2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-activities",
           title: "activities",
-          description: "Activities organized by the CS&amp;Law Europe Network. These monthly activities rotate and take place every last Friday of the month from 12.30-1.30 CET -- sign up with the Google sheet to get a calendar invite.",
+          description: "Activities organized by the CS&amp;Law Europe Network. These monthly activities rotate and take place every last Friday of the month from 12:30–13:30 CET -- sign up with the Google sheet to get a calendar invite.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/activities/";
@@ -19,7 +19,22 @@ ninja.data = [{
         },{id: "news-we-started-the-cs-amp-amp-law-european-network-with-a-kickoff-meeting-where-we-discussed-the-goals-and-objectives-of-the-network-and-future-events-to-achieve-them",
           title: 'We started the CS&amp;amp;amp;Law European Network with a kickoff meeting where we discussed...',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "talks-cleaning-the-launderette-lessons-from-3-years-of-the-european-open-source-ai-index",
+          title: 'Cleaning the Launderette: Lessons from 3 years of the European Open-Source AI Index...',
+          description: "",
+          section: "Talks",handler: () => {
+              window.location.href = "/talks/2026-09-25-learning-call/";
+            },},{id: "talks-topic-to-be-announced",
+          title: 'Topic to be announced',
+          description: "",
+          section: "Talks",handler: () => {
+              window.location.href = "/talks/2026-10-30-deep-dive-debate/";
+            },},{id: "talks-topic-to-be-announced",
+          title: 'Topic to be announced',
+          description: "",
+          section: "Talks",handler: () => {
+              window.location.href = "/talks/2026-11-27-learning-call/";
+            },},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
